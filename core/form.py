8fe749a6bb41da5c90db587ccc7a0a91695345ff
otavio <olima94@gmail.com>
2019-10-07 16:11:54 -0700

    deletando arquivos para organizar repositorio

--- core/form.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Book
-
-class Book_form(ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author']
--- core/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length= 40, blank=False, null=False)
-    author = models.CharField(max_length= 40)
