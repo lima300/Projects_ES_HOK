c4a22a52503b1524892940c3aa4f8ba94eef4964
Otavio de Lima Soares <olima94@gmail.com>
2019-12-06 12:41:48 -0800

    :hammer: fixa bug no alterar do crud de compras close: #8

--- core/tests.py
+++ core/tests.py
@@ -4,12 +4,12 @@ from core.models import Client
 
 class ClientTest(unittest.TestCase):
     def setUp(self):
-        self.jorge = Client.objects.create(first_name="Jorge", last_name="Silva", cpf="333.333.333-34", birth="1999-05-20", address="dajsgafbkvfd")
+        self.jorge = Client.objects.create(first_name="Jorge", last_name="Silva", cpf="333.333.333-36", birth="1999-05-20", address="dajsgafbkvfd")
 
     def testGetFullNAme(self):
         self.assertEquals(self.jorge.get_full_name(), 'Jorge Silva')
         self.assertEquals(self.jorge.get_full_name(), 'Jorge Carvalho')
 
 
-        
+
 
